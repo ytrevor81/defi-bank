f618a37514e8d8e01f7aa3b73f8f0345f30bbf80
Trevor Youngquist <ytrevor81@gmail.com>
2021-03-02 16:41:28 +0700

    defi bank frontend nearly finished

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import { Tabs, Tab } from 'react-bootstrap'
 import dBank from '../abis/dBank.json'
 import React, { Component } from 'react';
 import Token from '../abis/Token.json'
-import dbank from '../dbank.png';
 import Web3 from 'web3';
 import './App.css';
 
@@ -15,8 +14,37 @@ class App extends Component {
   }
 
   async loadBlockchainData(dispatch) {
+    if(typeof window.ethereum!=='undefined'){
+      const web3 = new Web3(window.ethereum);
+      const netId = await web3.eth.net.getId();
+      const accounts = await web3.eth.getAccounts();
 
-    //check if MetaMask exists
+      if(typeof accounts[0]!=='undefined'){
+        const balance = await web3.eth.getBalance(accounts[0]);
+        this.setState({ account: accounts[0], balance: balance, web3: web3 })
+
+        console.log(balance);
+      }
+      else {
+        window.alert('Please login to MetaMask.')
+      }
+
+      //load contracts
+      try {
+        const token = new web3.eth.Contract(Token.abi, Token.networks[netId].address);
+        const dbank = new web3.eth.Contract(dBank.abi, dBank.networks[netId].address);
+        const dBankAddress = dBank.networks[netId].address;
+        this.setState({ token: token, dbank: dbank, dBankAddress: dBankAddress });
+      } catch (e) {
+        console.log('Error', e);
+        window.alert('Contracts not deployed to the current network.');
+      }
+
+    }
+    else {
+      window.alert('This is a blockchain website. Please install the MetaMask browser extension to continue.')
+    }
+
 
       //assign to values to variables: web3, netId, accounts
 
@@ -53,21 +81,10 @@ class App extends Component {
   render() {
     return (
       <div className='text-monospace'>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-        <img src={dbank} className="App-logo" alt="logo" height="32"/>
-          <b>dBank</b>
-        </a>
-        </nav>
         <div className="container-fluid mt-5 text-center">
         <br></br>
-          <h1>{/*add welcome msg*/}</h1>
-          <h2>{/*add user address*/}</h2>
+          <h1>Welcome to Trev's DeFi Bank</h1>
+          <h3>Account: {this.state.account}</h3>
           <br></br>
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
